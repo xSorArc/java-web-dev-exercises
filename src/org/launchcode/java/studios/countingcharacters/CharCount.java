10f411af58fa5c33dd3966338d7a2de63953e8d7
xSorArc <87716789+xSorArc@users.noreply.github.com>
2022-08-25 20:07:01 -0500

    Added studio from Lesson 2

---
+++ src/org/launchcode/java/studios/countingcharacters/CharCount.java
@@ -0,0 +1,36 @@
+package org.launchcode.java.studios.countingcharacters;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class CharCount {
+    public static void main(String[] args) {
+        String hiddenFigures = "If the product of two terms is zero then common sense says at least one of the two " +
+                "terms has to be zero to start with. So if you move all the terms over to one side, you can put the " +
+                "quadratics into a form that can be factored allowing that side of the equation to equal zero. " +
+                "Once you’ve done that, it’s pretty straightforward from there.";
+
+//        cCount(hiddenFigures);
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter a string: ");
+        String userString = in.nextLine();
+
+        cCount(userString.toLowerCase());
+    }
+
+    public static HashMap cCount(String str) {
+        char[] charsInString = str.toCharArray();
+
+        HashMap<Character, Integer> count = new HashMap<>();
+
+        for (char c : charsInString) {
+            if (!count.containsKey(c)) {
+                count.put(c, 0);
+            }
+            count.put(c, count.get(c) + 1);
+        }
+        System.out.println(count);
+        return count;
+    }
+}
