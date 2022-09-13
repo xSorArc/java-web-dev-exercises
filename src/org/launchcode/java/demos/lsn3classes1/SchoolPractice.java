0004acabd3f6be738d88d1b7832cde4e7269cec7
xSorArc <87716789+xSorArc@users.noreply.github.com>
2022-09-13 13:47:17 -0500

    Lesson 3 updates

--- src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
+++ src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
@@ -1,7 +1,60 @@
 package org.launchcode.java.demos.lsn3classes1;
 
+import java.util.ArrayList;
+
 public class SchoolPractice {
     public static void main(String[] args) {
         // Instantiate your Student class for part 2 here!
+        Student me = new Student();
+
+        me.setName("Tom Sawyer");
+        me.setStudentId(001);
+        me.setNumberOfCredits(1);
+        me.setGpa(4.0);
+    }
+
+    public static class Course {
+        private String topic;
+        private Teacher instructor;
+        private ArrayList<Student> enrolledStudents;
+    }
+
+    public static class Teacher {
+        private String firstName;
+        private String lastName;
+        private String subject;
+        private int yearsTeaching;
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public String getSubject() {
+            return subject;
+        }
+
+        protected int getYearsTeaching() {
+            return yearsTeaching;
+        }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
+
+        public void setSubject(String subject) {
+            this.subject = subject;
+        }
+
+        public void setYearsTeaching(int yearsTeaching) {
+            this.yearsTeaching = yearsTeaching;
+        }
     }
 }
--- src/org/launchcode/java/demos/lsn3classes1/Student.java
+++ src/org/launchcode/java/demos/lsn3classes1/Student.java
@@ -10,4 +10,35 @@ public class Student {
     private int numberOfCredits = 0;
     private double gpa = 0.0;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String aName) {
+        this.name = aName;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int aStudentId) {
+        this.studentId = aStudentId;
+    }
+
+    public int getNumberOfCredits() {
+        return numberOfCredits;
+    }
+
+    protected void setNumberOfCredits(int aNumberOfCredits) {
+        this.numberOfCredits = aNumberOfCredits;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double aGpa) {
+        this.gpa = aGpa;
+    }
 }
